22c5d3682130dcd1cd1eb66cb457da7d92eadb1e
Julio Machado <juliocarlos00@hotmail.com>
2024-09-16 00:51:14 +0100

    add/ finish player page

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,10 +1,20 @@
 import { Header } from "@components/Header";
+import { Button } from "@components/Button";
 import { Highlight } from "@components/Highlight";
-import { Container, Form } from "./styles";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from "@components/Input";
+import { Filter } from "@components/Filter";
+import { FlatList } from "react-native";
+import { useState } from "react";
+
+import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles";
+import { PlayerCard } from "@components/PlayerCard";
+import { ListEmpty } from "@components/ListEmpty";
 
 export function Players() {
+  const [team, setTeam] = useState("Time A");
+  const [players, setPlayers] = useState<string[]>([]);
+
   return (
     <Container>
       <Header showBackButton />
@@ -19,6 +29,41 @@ export function Players() {
 
         <ButtonIcon icon="add" />
       </Form>
+
+      <HeaderList>
+        <FlatList
+          data={["Time A", "Time B"]}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+
+        <NumbersOfPlayers>{players?.length}</NumbersOfPlayers>
+      </HeaderList>
+
+      <FlatList
+        data={players}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <PlayerCard name={item} onRemove={() => {}} />
+        )}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Não há pessoas nesse time." />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          players.length === 0 && { flex: 1 },
+        ]}
+      />
+
+      <Button title="Remover Turma" type="SECONDARY" />
     </Container>
   );
 }
