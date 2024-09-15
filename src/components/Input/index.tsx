beb53b621a812acc9ff38252368f7126598f5ccc
Julio Machado <juliocarlos00@hotmail.com>
2024-09-15 23:56:41 +0100

    fix/css and use css helper

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,8 +1,13 @@
 import { TextInputProps } from "react-native";
+import { useTheme } from "styled-components/native";
+
 import { Container } from "./styles";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 type InputProps = TextInputProps & {};
 
 export function Input({ ...rest }: InputProps) {
-  return <Container {...rest} />;
+  const { COLORS } = useTheme();
+
+  return <Container placeholderTextColor={COLORS.GRAY_300} {...rest} />;
 }
